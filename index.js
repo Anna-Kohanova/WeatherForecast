59d9f2b3c0c2bb8ff1244d2124d79b44c357a27d
Anna Kohanova <anitacx88@gmail.com>
2017-03-14 15:14:40 +0300

    Update index.js
    Add error 404 handler.

--- index.js
+++ index.js
@@ -13,4 +13,10 @@ var forecast = require('./router/forecast');
 app.use('/current', current);
 app.use('/forecast', forecast);
 
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 app.listen(localConfig.app.port);
