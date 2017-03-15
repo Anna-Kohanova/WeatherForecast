87e110de5bb1ea620a8fee0b101a85ca2a94d40e
Anna-Kohanova <anitacx88@gmail.com>
2017-03-15 19:04:29 +0300

    Add LevelUP storage

--- handlers.js
+++ handlers.js
@@ -1,11 +1,40 @@
 var config = require('./config');
 var url = require('url');
 var http = require('http');
+var levelup = require('level')
+
+ сurrentDB = levelup('./currentDB');
+forecastDB = levelup('./forecastDB');
+
 function current(request, response){
-    makeQuery(request,response, 'weather');
+    сurrentDB.get(request.params.city, function(err, value){
+        if (err) {
+    if (err.notFound) {
+      console.log('not found');
+        makeQuery(request,response, 'weather');
+      return;
+    }
+    // I/O or other error, pass it up the callback chain
+    return err;
+  }
+
+    response.json(value);
+    })
 }
 function forecast(request, response){
-    makeQuery(request,response, 'forecast');
+    forecastDB.get(request.params.city, function(err, value){
+        if (err) {
+    if (err.notFound) {
+      console.log('not found');
+        makeQuery(request,response, 'forecast');
+      return;
+    }
+    // I/O or other error, pass it up the callback chain
+    return err;
+  }
+
+    response.json(value);
+    })
 }
 function makeQuery(request,response, type) {
     var city = request.params.city;
@@ -22,9 +51,21 @@ function makeQuery(request,response, type) {
             responseCallback.on('end', function () {
                 var weather = JSON.parse(str);
                 response.json(str);
+                if(type='weather'){
+                    сurrentDB.put(city, str, function(err){
+                    if (err) return console.log(err);
+                });
+                }
+                else {
+                    forecastDB.put(city, str, function(err){
+                    if (err) return console.log(err);
+                });
+                }
+
             });
         }
         http.request(options, callback).end(); 
+    
 }
 
 
--- index.js
+++ index.js
@@ -14,3 +14,8 @@ app.use('/current', current);
 app.use('/forecast', forecast);
 
 app.listen(localConfig.app.port);
+//
+var crontab = require('node-crontab');
+var jobId = crontab.scheduleJob("*/2 * * * *", function(){ //This will call this function every 2 minutes 
+    console.log("It's been 2 minutes!");
+});
